40785ebaa7c9e3fa7faaca8eaf5cdd8e081ac58a
Ellie Tomkins <etomkins@banlaw.com>
2018-01-23 14:27:35 +1100

    Simple mini controller window for spotify.

---
+++ MicroSpot/ImageExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace MicroSpot
+{
+    public static class ImageExtensions
+    {
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);    // https://stackoverflow.com/a/1546121/194717
+
+        /// <summary> 
+        /// Converts a <see cref="System.Drawing.Bitmap"/> into a WPF <see cref="BitmapSource"/>. 
+        /// </summary> 
+        /// <remarks>Uses GDI to do the conversion. Hence the call to the marshalled DeleteObject. 
+        /// </remarks> 
+        /// <param name="source">The source bitmap.</param> 
+        /// <returns>A BitmapSource</returns> 
+        public static BitmapSource ToBitmapSource(this Bitmap source)
+        {
+            var hBitmap = source.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                DeleteObject(hBitmap);
+            }
+        }
+    }
+}
--- MicroSpot/MainWindow.xaml.cs
+++ MicroSpot/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Newtonsoft.Json;
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Enums;
+using SpotifyAPI.Local.Models;
 
 namespace MicroSpot
 {
@@ -20,9 +15,90 @@ namespace MicroSpot
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly SpotifyLocalAPI spotify;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            spotify = new SpotifyLocalAPI();
+            if (!SpotifyLocalAPI.IsSpotifyRunning())
+                return; //Make sure the spotify client is running
+            if (!SpotifyLocalAPI.IsSpotifyWebHelperRunning())
+                return; //Make sure the WebHelper is running
+
+            if (!spotify.Connect())
+                return; //We need to call Connect before fetching infos, this will handle Auth stuff
+
+            var status = spotify.GetStatus(); //status contains infos
+            Debug.WriteLine($"Current status: {JsonConvert.SerializeObject(status)}");
+            UpdateDisplay(status.Track);
+
+            spotify.OnTrackChange += OnTrackChange;
+            spotify.OnPlayStateChange += OnPlayStateChange;
+            spotify.ListenForEvents = true;
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+        }
+
+        private void OnPlayStateChange(object sender, PlayStateEventArgs playStateEventArgs)
+        {
+            Debug.WriteLine($"PlayState changed: {JsonConvert.SerializeObject(playStateEventArgs)}");
+        }
+
+        private void OnTrackChange(object sender, TrackChangeEventArgs trackChangeEventArgs)
+        {
+            Debug.WriteLine($"Track changed: {JsonConvert.SerializeObject(trackChangeEventArgs)}");
+            Dispatcher.BeginInvoke(new Action(() => { UpdateDisplay(trackChangeEventArgs.NewTrack); }));
+        }
+
+        private async void UpdateDisplay(Track track)
+        {
+            TrackTitle.Text = track.TrackResource.Name;
+            TrackArtist.Text = track.ArtistResource.Name;
+            TrackImage.Source = track.AlbumResource != null ? (await track.GetAlbumArtAsync(AlbumArtSize.Size160)).ToBitmapSource() : null;
+        }
+
+        private void OnBackClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                spotify.Previous();
+            }
+        }
+
+        private async void OnPlayPauseClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                var status = spotify.GetStatus();
+                if (status.Playing)
+                {
+                    await spotify.Pause();
+                    btn.Content = "|>";
+                }
+                else
+                {
+                    await spotify.Play();
+                    btn.Content = "||";
+                }
+            }
+        }
+
+        private void OnNextClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                spotify.Skip();
+            }
+        }
+
+        private void OnWindowMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
         }
     }
 }
